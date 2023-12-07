9f6c40e6b6b104c8431d7554227225b8281c97c8
exploring-solver <amansharma12607@gmail.com>
2023-12-07 12:03:32 +0530

    something

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'timetable',
+    'user_management',
+    '',
 ]
 
 MIDDLEWARE = [
